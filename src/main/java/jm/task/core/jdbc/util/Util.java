c73c6cb181681ad6c5ebd60bec259c2aa0a50df5
rizebutrise <devjatajaa@gmail.com>
2025-08-01 19:21:38 +0300

    решение 1.1.4 без xml (испр)

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,6 +6,7 @@ import jm.task.core.jdbc.model.User;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Configuration;
+
 import java.sql.*;
 
 public class Util {
@@ -21,23 +22,27 @@ public class Util {
         }
     }
 
-    private static final SessionFactory sessionFactory = buildSessionFactory();
+    private static final SessionFactory sessionFactory;
 
-    private static SessionFactory buildSessionFactory() {
+    static {
         try {
-            return new Configuration()
-                    .configure("hibernate.cfg.xml")
-                    .addAnnotatedClass(User.class)
-                    .buildSessionFactory();
+            Configuration configuration = new Configuration()
+                    .setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver")
+                    .setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/mydbtest")
+                    .setProperty("hibernate.connection.username", "root")
+                    .setProperty("hibernate.connection.password", "sT85zzQLg3011")
+                    .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+                    .setProperty("hibernate.show_sql", "true")
+                    .setProperty("hibernate.hbm2ddl.auto", "update")
+                    .addAnnotatedClass(User.class);
+
+            sessionFactory = configuration.buildSessionFactory();
         } catch (Throwable ex) {
             throw new ExceptionInInitializerError(ex);
         }
     }
+
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
-    }
-
-    public static Connection getJDBCConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
     }
 }
